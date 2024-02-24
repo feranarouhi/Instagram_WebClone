0f86260f9a103701cca249aaf18beeef3f1f59d4
ferana rouhi <124641424+feranarouhi@users.noreply.github.com>
2024-02-24 02:23:58 -0500

    Add files via upload

---
+++ instagram clone/modules/Bio.js
@@ -0,0 +1,14 @@
+const  Bio = () => 
+    ` 
+<section class="Bio"> 
+    <div class="profile-photo"><img src="https://yt3.googleusercontent.com/kc0yqeSdLMozjp-wKEk3xsxP559ju9j9ADglssIhDkxykdglAHroajSPkROeNOBOoLJ1gpNMRg=s900-c-k-c0x00ffffff-no-rj"
+         alt="profile-photo">
+    </div>
+    <div class="profile-info">
+        <p class="username">Ferana Rouhi</p>
+        <button>follow</button>
+    </div>
+</section> `
+
+export default Bio;
+ 
---
+++ instagram clone/modules/Gallery.js
@@ -0,0 +1,26 @@
+const Gallery = () => 
+    ` 
+    <section class="gallery"> 
+              
+    <a href = "#" class="item">
+        <img src="https://images.pexels.com/photos/6773216/pexels-photo-6773216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=" ">
+    </a>
+    <a href = "#" class="item">
+        <img src="https://images.pexels.com/photos/6373853/pexels-photo-6373853.jpeg?auto=compress&cs=tinysrgb&w=800" alt=" ">
+    </a>
+    <a href = "#" class="item">
+        <img src="https://images.pexels.com/photos/7179056/pexels-photo-7179056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=" ">
+    </a>
+    <a href = "#" class="item">
+        <img src="https://images.pexels.com/photos/4008340/pexels-photo-4008340.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=" ">
+    </a> 
+    <a href = "#" class="item">
+        <img src="https://images.pexels.com/photos/17945059/pexels-photo-17945059/free-photo-of-brunette-woman-posing-in-black-blazer-jeans-and-boots.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=" ">
+    </a>
+    <a href = "#" class="item">
+        <img src="https://images.pexels.com/photos/7578202/pexels-photo-7578202.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=" ">
+    </a> 
+</section> 
+` 
+
+export default Gallery;
